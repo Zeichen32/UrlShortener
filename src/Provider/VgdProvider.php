23c792880d29846e6c18a1a59aba76fb54db0ff8
Jens Averkamp <j.averkamp@two-developers.com>
2015-04-18 01:41:09 +0200

    Fix vgd default max_results

--- src/Provider/VgdProvider.php
+++ src/Provider/VgdProvider.php
@@ -20,6 +20,7 @@ class VgdProvider extends IsgdProvider
         parent::configureOptions($optionsResolver);
         $optionsResolver->setDefaults([
             'endpoint' => 'http://v.gd/',
+            'max_results' => self::DEFAULT_MAX_RESULTS,
         ]);
     }
 
